f9b25b96db086358dce9783543b1d3f4e33553a8
Nilson Nascimento <nilson@chuva.io>
2018-05-26 21:50:35 -0400

    Prepare view controller
    Add image picking

--- meetup_1-2/ViewController.swift
+++ meetup_1-2/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var confidenceLabel: UILabel!
+    @IBOutlet weak var classificationLabel: UILabel!
+    @IBOutlet weak var selectedImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        confidenceLabel.text = ""
+        classificationLabel.text = ""
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIBarButtonItem) {
+        confidenceLabel.text = nil
+        classificationLabel.text = nil
+        selectedImage.image = nil
+    }
+    
+    @IBAction func selectImageTapped(_ sender: UIBarButtonItem) {
+        let sources: [UIImagePickerControllerSourceType] = [.photoLibrary,
+                                                            .camera,
+                                                            .savedPhotosAlbum]
+        
+        let mediaTypes = sources.map { UIImagePickerController.availableMediaTypes(for: $0) ?? [] }
+            .reduce([]) { $0 + $1 }
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.mediaTypes = mediaTypes
+        present(vc, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+} 
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage]! as! UIImage
+        selectedImage.image = image
+        picker.dismiss(animated: true)
+        
     }
-
-
+    
 }
-
