88c7ecbe79c6a9229432163846f94009fb471487
Nilson Nascimento <nilson@chuva.io>
2018-05-26 22:38:41 -0400

    Refactor to use Vision framework

--- meetup_1-2/ViewController.swift
+++ meetup_1-2/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
 
@@ -52,58 +53,21 @@ extension ViewController: UIImagePickerControllerDelegate {
         selectedImage.image = image
         picker.dismiss(animated: true)
         
-        let size = 299
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: size, height: size), true, 1.0)
-        image.draw(in: CGRect(x: 0,
-                              y: 0,
-                              width: size,
-                              height: size))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
         
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
-                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer: CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault,
-                                         Int(newImage.size.width),
-                                         Int(newImage.size.height),
-                                         kCVPixelFormatType_32ARGB,
-                                         attrs,
-                                         &pixelBuffer)
-        
-        guard (status == kCVReturnSuccess) else { return }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixelData,
-                                width: Int(newImage.size.width),
-                                height: Int(newImage.size.height),
-                                bitsPerComponent: 8,
-                                bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
-                                space: rgbColorSpace,
-                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-        
-        context?.translateBy(x: 0, y: newImage.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context!)
-        newImage.draw(in: CGRect(x: 0,
-                                 y: 0,
-                                 width: newImage.size.width,
-                                 height: newImage.size.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        
-        selectedImage.image = newImage
-        
-        let model = Inceptionv3()
-        guard let prediction = try? model.prediction(image: pixelBuffer!) else { return }
-        classificationLabel.text = prediction.classLabel
-        let confidence = prediction.classLabelProbs[prediction.classLabel]!
-        confidenceLabel.text = String(format: "%0.2f", confidence * 100) + " %"
-        
+        let model = try! VNCoreMLModel(for: Inceptionv3().model)
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!)
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard error == nil else {
+                self.confidenceLabel.text = nil
+                self.classificationLabel.text = "CLASSIFICATION FAILED"
+                return
+            }
+            let result = request.results!.first as! VNClassificationObservation
+            self.classificationLabel.text = result.identifier
+            self.confidenceLabel.text = String(format: "%0.2f %", result.confidence * 100) + " %"
+        }
+        try! handler.perform([request])
+
     }
     
 }
