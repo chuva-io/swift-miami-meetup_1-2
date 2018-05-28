912baa334792293b5bc839c97a8138929ccfff6d
Nilson Nascimento <nilson@chuva.io>
2018-05-28 19:51:03 -0400

    Add word detection

--- meetup_1-2/TextRecognitionVC.swift
+++ meetup_1-2/TextRecognitionVC.swift
@@ -8,41 +8,78 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class TextRecognitionVC: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
     let captureSession = AVCaptureSession()
+    var textDetectionRequest: VNDetectTextRectanglesRequest!
+    lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // input
+        // session input
         let captureDevice = AVCaptureDevice.default(for: .video)!
         let input = try! AVCaptureDeviceInput(device: captureDevice)
         
-        // output
+        // session output
         captureSession.sessionPreset = .photo
         let output = AVCaptureVideoDataOutput()
-        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA] // kCVPixelFormatType_32RGBA
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)] // kCVPixelFormatType_32RGBA
         output.setSampleBufferDelegate(self, queue: DispatchQueue.global())
         
         captureSession.addInput(input)
         captureSession.addOutput(output)
         
-        let image = AVCaptureVideoPreviewLayer(session: captureSession)
-        image.frame = imageView.bounds
-        imageView.layer.addSublayer(image)
+        let imageLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        imageLayer.frame = imageView.bounds
+        imageView.layer.addSublayer(imageLayer)
         
         captureSession.startRunning()
+        
+        // text detection
+        textDetectionRequest = VNDetectTextRectanglesRequest { request, error in
+            DispatchQueue.main.async {
+                self.imageView.layer.sublayers?.removeSubrange(1...) // clear results
+                let results = request.results as? [VNTextObservation] ?? []
+                self.highlightWords(textObservations: results)
+            }
+        }
+    }
+    
+    func highlightWords(textObservations: [VNTextObservation]) {
+        for result in textObservations {
+            let border = CALayer()
+            border.borderWidth = 2.0
+            border.borderColor = UIColor.red.cgColor
+            border.frame = transformedFrame(frame: result.boundingBox)
+            
+            imageView.layer.addSublayer(border)
+        }
+    }
+    
+    func transformedFrame(frame: CGRect) -> CGRect {
+        return CGRect(x: frame.minX * imageView.bounds.width,
+                      y: (1 - frame.maxY) * imageView.bounds.height,
+                      width: frame.width * imageView.bounds.width,
+                      height: frame.height * imageView.bounds.height)
     }
     
 }
 
 
 extension TextRecognitionVC: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer,
+                                            orientation: .right,
+                                            options: [:])
+        try? handler.perform([textDetectionRequest])
     }
+    
 }
